0d3caf0eadac2f62882eb55389ea3b07ff3994e2
olton <serhii@pimenov.com.ua>
2024-10-16 20:34:34 +0300

    add default options

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import {unlinkSync, existsSync} from "fs";
-import { glob } from "glob";
+import { globSync } from "glob";
+
+const defaults = {
+    files: "**/*",
+    skip: '',
+}
 
 export default options => {
+    const o = {...defaults, ...options};
+
     return {
         name: 'unlink',
         setup(build) {
-            build.onEnd(async () => {
-                const files = await glob(options.files, { ignore: options.skip });
+            build.onEnd(() => {
+                const files = globSync(o.files, { ignore: o.skip });
                 for (const file of files) {
                     if (existsSync(file)) {
                         unlinkSync(file);
